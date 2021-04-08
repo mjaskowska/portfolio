1e41d2fcec01da93673e033c54235e3ca7faeae7
Magda <jaskowska.magda@gmail.com>
2021-04-08 18:45:11 +0100

    projects content

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,68 +1,106 @@
-import React from 'react'
+import React from "react";
 import { FaFile, FaFileCode } from "react-icons/fa";
 
-import trulse from './../../assets/trulse.png'
-
+import trulse from "./../../assets/trulse.png";
+import { experience, personalProjects } from "./info";
 
 const Projects = () => {
-    return (
-        <div className="projects">
-            <h3 className="subtitle"><span>03.</span> Experience & Projects</h3>
-            <section className="experience">
-                <h4 className="mid-title">Experience:</h4>
-                <h5 className="company-name">Open Source Project <span>from 03/2021</span></h5>
-                <p className="job-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem cumque quo veritatis ratione fugit sequi ipsum accusantium odio nostrum!</p>
-                <h5 className="project-title">SARO</h5>
-                <div className="project-container">
-                    <div className="project-img">
-                        <img src={trulse} alt=""/>
-                        <div className="links-container">
-                        <a href="/"><FaFile /></a>
-                        <a href="/"><FaFileCode/></a>
-                        </div>
-                    </div>
-                    <div className="project-info">
-                        <section className="project-description">
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, possimus?</p>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, possimus?</p>
-                        </section>
-                        <ul className="project-tech">
-                                <li>HTML</li>
-                                <li>HTML</li>
-                                <li>HTML</li>
-                                <li>HTML</li>
-                        </ul>
-                    </div>
+  return (
+    <div className="projects">
+      <h3 className="subtitle">
+        <span>03.</span> Experience & Projects
+      </h3>
+      <section className="experience">
+        <h4 className="mid-title">Experience:</h4>
+        {experience.map((item) => {
+          const {
+            companyName,
+            timeline,
+            jobDescription,
+            projectTitle,
+            imgURL,
+            demoURL,
+            githubURL,
+            description,
+            tech,
+          } = item;
+          return (
+            <>
+              <h5 className="company-name">
+                {companyName} <span>{timeline}</span>
+              </h5>
+              <p className="job-description">{jobDescription}</p>
+              <h5 className="project-title">{projectTitle}</h5>
+              <div className="project-container">
+                <div className="project-img">
+                  <img src={imgURL} alt={projectTitle} />
+                  <div className="links-container">
+                    <a href={demoURL}>
+                      <FaFile />
+                    </a>
+                    <a href={githubURL}>
+                      <FaFileCode />
+                    </a>
+                  </div>
                 </div>
-            </section>
-            <section className="personal-projects">
-                <h4 className="mid-title">Personal projects:</h4>               
-                <h5 className="project-title">SARO</h5>
-                <div className="project-container">
-                    <div className="project-img">
-                        <img src={trulse} alt=""/>
-                        <div className="links-container">
-                        <a href="/"><FaFile /></a>
-                        <a href="/"><FaFileCode/></a>
-                        </div>
-                    </div>
-                    <div className="project-info">
-                        <section className="project-description">
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, possimus?</p>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, possimus?</p>
-                        </section>
-                        <ul className="project-tech">
-                                <li>HTML</li>
-                                <li>HTML</li>
-                                <li>HTML</li>
-                                <li>HTML</li>
-                        </ul>
-                    </div>
+                <div className="project-info">
+                  <section className="project-description">
+                    <p>{description}</p>
+                  </section>
+                  <ul className="project-tech">
+                  {tech.map((item) => <li>{item}</li>)}
+                  </ul>
                 </div>
-            </section>
-            <button className="orange-btn"><a href="https://github.com/mjaskowska">See more on GitHub</a></button>
-        </div>
-    )
-}
+              </div>
+            </>
+          );
+        })}
+      </section>
+      <section className="personal-projects">
+        <h4 className="mid-title">Personal projects:</h4>
+        {personalProjects.map((item) => {
+          const {
+            projectTitle,
+            imgURL,
+            demoURL,
+            githubURL,
+            description,
+            tech,
+          } = item;
+          return (
+            <>
+              <h5 className="project-title">{projectTitle}</h5>
+              <div className="project-container">
+                <div className="project-img">
+                  <img src={imgURL} alt={projectTitle} />
+                  <div className="links-container">
+                    <a href={demoURL}>
+                      <FaFile />
+                    </a>
+                    <a href={githubURL}>
+                      <FaFileCode />
+                    </a>
+                  </div>
+                </div>
+                <div className="project-info">
+                  <section className="project-description">
+                    <p>{description}
+                    </p>
+                  </section>
+                  <ul className="project-tech">
+                      {tech.map((item) => <li>{item}</li>)}
+                  </ul>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </section>
+      <button className="orange-btn">
+        <a href="https://github.com/mjaskowska">See more on GitHub</a>
+      </button>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
---
+++ src/components/projects/info.js
@@ -0,0 +1,49 @@
+import trulse from "./../../assets/trulse.png";
+
+export const experience = [
+    {
+      companyName: "Open Source Project",
+      timeline: "from 03/2021",
+      jobDescription:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem cumque quo veritatis ratione fugit sequi ipsum accusantium odio nostrum!",
+      projectTitle: "SARO",
+      imgURL:  trulse ,
+      demoURL: "/",
+      githubURL: "/",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem cumque quo veritatis ratione fugit sequi ipsum accusantium odio nostrum!",
+      tech: ["HTML", "HTML", "HTML", "HTML", "HTML"],
+    }, 
+
+  ];
+
+  export const personalProjects = [
+    {
+        projectTitle: "SARO",
+        imgURL: trulse,
+        demoURL: "/",
+        githubURL: "/",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem cumque quo veritatis ratione fugit sequi ipsum accusantium odio nostrum!",
+        tech: ["HTML", "HTML", "HTML", "HTML", "HTML"],
+      },
+      {
+        projectTitle: "SARO",
+        imgURL: trulse,
+        demoURL: "/",
+        githubURL: "/",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem cumque quo veritatis ratione fugit sequi ipsum accusantium odio nostrum!",
+        tech: ["HTML", "HTML", "HTML", "HTML", "HTML"],
+      },
+      {
+        projectTitle: "SARO",
+        imgURL: trulse,
+        demoURL: "/",
+        githubURL: "/",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem cumque quo veritatis ratione fugit sequi ipsum accusantium odio nostrum!",
+        tech: ["HTML", "HTML", "HTML", "HTML", "HTML"],
+      },
+  ]
+  
